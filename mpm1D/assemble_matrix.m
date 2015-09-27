25b67208aa89bc4897df66ca3f5ba2a998fc7d9e
ewob <lisa_wobbes@hotmail.com>
2015-09-27 17:27:03 +0200

    Add mpm1D/

---
+++ mpm1D/assemble_matrix.m
@@ -0,0 +1,21 @@
+function [M] = assemble_matrix (n_n, n_e, elements_index, A)
+
+%% Boolean matrices required for the assemblage procedure
+T = zeros(2, n_n, n_e);
+for j=1:n_e
+    T(1,elements_index(j,1),j) = 1;
+    T(2,elements_index(j,2),j) = 1;
+end
+
+
+%assemble matrix
+M = T(:,:,1)'*A(:,:,1)*T(:,:,1);
+for j = 2:n_e
+    Temp = T(:,:,j)'*A(:,:,j)*T(:,:,j);
+    M = M+Temp;
+    clear Temp
+end
+
+end
+
+
