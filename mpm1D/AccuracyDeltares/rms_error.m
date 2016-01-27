ead62557069564258cc4c91461ceee1949a40c09
ewob <lisa_wobbes@hotmail.com>
2016-01-27 15:44:18 +0100

    accuracy FEM

--- mpm1D/AccuracyDeltares/rms_error.m
+++ mpm1D/AccuracyDeltares/rms_error.m
@@ -1,7 +1,8 @@
 function [] = rms_error()
 %% Provide required info
 % initial particle positions
-x0 = readvtk('x_init_mpm_8_128.txt');
+x0 = read_position('x_init_fem_128.txt');
+x0 = x0*10^(-8);
 
 %x01 = 
 % other parameters
@@ -17,8 +18,8 @@ n = T/t_step;
 x_exact = exact_solution(x0, n, t_step, v0, E, rho, L);
 
 %% Read the numerical solution
-x_num = readvtk('x_end_mpm_8_128.txt');
-
+x_num = read_position('x_end_fem_128.txt');
+x_num = x_num*10^(-8);
 %% Compute the RMS error
 number_particles = size(x0,1);
 Error = norm((x_exact-x_num))/sqrt(number_particles)
